0a748708706247c51c8605a1ef3bccc39e5af3fc
soogyeong <97sg0215@gmail.com>
2018-05-17 01:03:38 +0900

    loginActDesign_ing

---
+++ app/src/main/java/graduationwork/buskingtown/SignUp1Step.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp1Step extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up1_step);
+    }
+}
