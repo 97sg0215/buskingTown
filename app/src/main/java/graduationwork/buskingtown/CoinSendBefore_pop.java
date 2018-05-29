52fe84375c763cbf67e848a5e2f1ed0e02d22685
jw990 <jw9903@naver.com>
2018-05-29 19:27:47 +0900

    background 흰색으로 바꿈 코인 보내기 전 팝업창 생성 버스커 생성 창 생성

---
+++ app/src/main/java/graduationwork/buskingtown/CoinSendBefore_pop.java
@@ -0,0 +1,14 @@
+package graduationwork.buskingtown;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoinSendBefore_pop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_send_before_pop);
+    }
+}
