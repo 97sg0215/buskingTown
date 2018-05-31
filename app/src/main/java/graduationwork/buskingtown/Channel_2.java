a0abaef95365e8e6084f49591b36197f827852e5
gktjd <gktjdrud@gmail.com>
2018-05-31 22:13:30 +0900

    일반인 채널

---
+++ app/src/main/java/graduationwork/buskingtown/Channel_2.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Channel_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_channel_2);
+    }
+}
