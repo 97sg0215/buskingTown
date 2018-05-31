c6e918f484bd5b4e074712a399c1e78b7450de27
minnn21 <minnn21@naver.com>
2018-05-31 22:20:05 +0900

    갤러리에서 사진 선택해서 이미지뷰에 넣기

--- app/src/main/java/graduationwork/buskingtown/BuskerCertification.java
+++ app/src/main/java/graduationwork/buskingtown/BuskerCertification.java
@@ -1,13 +1,76 @@
 package graduationwork.buskingtown;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class BuskerCertification extends AppCompatActivity {
+
+    final int REQ_CODE_SELECT_IMAGE=100;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busker_certification);
+
+        ImageView imgChoice = (ImageView) findViewById(R.id.imageIcon);
+        imgChoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
+
+
+
+
+            }
+        });
+
     }
+
+
+
+    // 선택된 이미지 가져오기
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQ_CODE_SELECT_IMAGE)
+        {
+            if(resultCode== Activity.RESULT_OK)
+            {
+                try {
+                    //Uri에서 이미지 이름을 얻어온다.
+                    //String name_Str = getImageNameToUri(data.getData());
+
+                    //이미지 데이터를 비트맵으로 받아온다.
+                    Bitmap image_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+
+                    //배치해놓은 ImageView에 set
+                    ImageView imageS = (ImageView)findViewById(R.id.imageIcon);
+                    imageS.setImageBitmap(image_bitmap);
+
+                }catch (FileNotFoundException e) { e.printStackTrace(); }
+
+                catch (IOException e) { e.printStackTrace(); }
+
+                catch (Exception e) { e.printStackTrace();	}
+
+            }
+
+        }
+
+    }
+
+
 }
