1152cddbd05586e84dfd3c3f1caaf9aa336aecb7
jw990 <jw9903@naver.com>
2018-06-02 18:01:51 +0900

    CoinSendBefore_pop(팝업창)수정 coinbig이미지 추가

--- app/src/main/java/graduationwork/buskingtown/CoinSendBefore_pop.java
+++ app/src/main/java/graduationwork/buskingtown/CoinSendBefore_pop.java
@@ -3,12 +3,14 @@ package graduationwork.buskingtown;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class CoinSendBefore_pop extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_coin_send_before_pop);
     }
 }
