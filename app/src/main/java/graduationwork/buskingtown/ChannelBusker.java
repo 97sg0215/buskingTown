ded03cc99030a4368640528a16108a4e99ec06ec
gktjd <gktjdrud@gmail.com>
2018-06-06 20:47:50 +0900

    channel_user tab분해

--- app/src/main/java/graduationwork/buskingtown/ChannelBusker.java
+++ app/src/main/java/graduationwork/buskingtown/ChannelBusker.java
@@ -2,6 +2,7 @@ package graduationwork.buskingtown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class ChannelBusker extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ChannelBusker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channel_busker);
+
+        ImageButton backBtn = (ImageButton) findViewById(R.id.backBtn);
+
     }
 }
