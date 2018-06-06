dd01f19b4f7e96a730ce7d5a39534a2cfc319281
gktjd <gktjdrud@gmail.com>
2018-06-06 21:07:38 +0900

    channel_user tab fragment2 그려줘랑 정우야

---
+++ app/src/main/java/graduationwork/buskingtown/Channel_user_fragment1.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Channel_user_fragment1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_channel_user_fragment1);
+    }
+}
