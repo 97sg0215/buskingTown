03facbf49e8ffd73c0902ed12b11fdc9606b3504
soogyeong <97sg0215@gmail.com>
2018-06-07 23:58:32 +0900

    채널탭 수정

--- app/src/main/java/graduationwork/buskingtown/ChannelBuskerReservation.java
+++ app/src/main/java/graduationwork/buskingtown/ChannelBuskerReservation.java
@@ -3,11 +3,11 @@ package graduationwork.buskingtown;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Channel_busker_fragment1 extends AppCompatActivity {
+public class ChannelBuskerReservation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_channel_busker_fragment1);
+        setContentView(R.layout.activity_channel_busker_reservation);
     }
 }
--- app/src/main/java/graduationwork/buskingtown/ChannelBuskerSchedule.java
+++ app/src/main/java/graduationwork/buskingtown/ChannelBuskerSchedule.java
@@ -3,11 +3,11 @@ package graduationwork.buskingtown;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Channel_busker_fragment2 extends AppCompatActivity {
+public class ChannelBuskerSchedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_channel_busker_fragment2);
+        setContentView(R.layout.activity_channel_busker_schedule);
     }
 }
