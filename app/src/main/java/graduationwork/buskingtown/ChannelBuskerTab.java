5630f2b14ba308d676b5de13ad826865b86b66d4
Hongjeongwoo <0721baby@gmail.com>
2018-06-08 16:53:06 +0900

    버스커 채널 탭 추가

---
+++ app/src/main/java/graduationwork/buskingtown/ChannelBuskerTab.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChannelBuskerTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_channel_busker_tab);
+    }
+}
