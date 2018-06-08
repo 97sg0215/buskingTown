dc18820d293920cf19f68f281d1ec84f7d9eee69
minnn21 <minnn21@naver.com>
2018-06-08 17:04:20 +0900

    콘서트장, 연습실 탭 프래그먼트로 수정

--- app/src/main/java/graduationwork/buskingtown/ChannelBuskerConcert.java
+++ app/src/main/java/graduationwork/buskingtown/ChannelBuskerConcert.java
@@ -1,13 +1,10 @@
 package graduationwork.buskingtown;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ChannelBuskerConcert extends AppCompatActivity {
+public class ChannelBuskerConcert extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_channel_busker_concert);
-    }
+
 }
--- app/src/main/java/graduationwork/buskingtown/ChannelBuskerReservation.java
+++ app/src/main/java/graduationwork/buskingtown/ChannelBuskerReservation.java
@@ -6,8 +6,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ChannelBuskerReservation extends Fragment {
+    //호출될프래그먼트 변수들
+    private final int roadFRAGMENT = 1;
+    private final int practicePlaceFRAGMENT = 2;
+    private final int concertPlaceFRAGMENT = 3;
+
+    //탭바 아이콘 변수들
+    private TextView roadConcert, practiceRoom, concert;
+
     public ChannelBuskerReservation(){
         // Required empty public constructor
     }
@@ -17,6 +27,8 @@ public class ChannelBuskerReservation extends Fragment {
         final View v = inflater.inflate(R.layout.activity_channel_busker_reservation, container, false);
 
         return v;
+
+        // 프래그먼트는 getView해서 찾아와야함 !!!!
     }
 
 }
