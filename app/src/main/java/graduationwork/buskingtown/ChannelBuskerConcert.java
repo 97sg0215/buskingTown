4a322fcbb2230f1e20abe5ac15f53f27ef78b3ca
gktjd <gktjdrud@gmail.com>
2018-06-08 18:27:33 +0900

    channel_busker 레이아웃수정

--- app/src/main/java/graduationwork/buskingtown/ChannelBuskerConcert.java
+++ app/src/main/java/graduationwork/buskingtown/ChannelBuskerConcert.java
@@ -3,6 +3,7 @@ package graduationwork.buskingtown;
 import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class ChannelBuskerConcert extends Fragment {
 
