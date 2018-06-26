1a7952beff15d68e40a73ce7e91699e1e7b6848d
Hongjeongwoo <0721baby@gmail.com>
2018-06-26 15:15:27 +0900

    코인충전소, 버전정보, 비밀번호 변경 액티비티 생성 이미지 추가 메인화면 생성

---
+++ app/src/main/java/graduationwork/buskingtown/PasswdChange.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PasswdChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passwd_change);
+    }
+}
