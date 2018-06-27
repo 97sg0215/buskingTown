e07f0cae054b54af16777779b78b8aedf21cf981
gktjd <gktjdrud@gmail.com>
2018-06-28 01:30:01 +0900

    고객센터 그림

---
+++ app/src/main/java/graduationwork/buskingtown/Customer_center.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Customer_center extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_center);
+    }
+}
