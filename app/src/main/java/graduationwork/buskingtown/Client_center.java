07eae2380ca3fea42ebefca267528e8517a8e61a
gktjd <gktjdrud@gmail.com>
2018-06-28 14:22:47 +0900

    client_center 고객센터

--- app/src/main/java/graduationwork/buskingtown/Client_center.java
+++ app/src/main/java/graduationwork/buskingtown/Client_center.java
@@ -3,11 +3,11 @@ package graduationwork.buskingtown;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Customer_center extends AppCompatActivity {
+public class Client_center extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_center);
+        setContentView(R.layout.activity_client_center);
     }
 }
