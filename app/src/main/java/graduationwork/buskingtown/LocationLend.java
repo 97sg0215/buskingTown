a0aed7bde7a2b614dff3caca14fbb6119ef7b57c
Hongjeongwoo <0721baby@gmail.com>
2018-07-03 05:07:35 +0900

    장소빌려주기, 프로필 편집 생성 사진 추가 openconcert액티비티 수정 마이페이지 사진 수정

---
+++ app/src/main/java/graduationwork/buskingtown/LocationLend.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationLend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_lend);
+    }
+}
