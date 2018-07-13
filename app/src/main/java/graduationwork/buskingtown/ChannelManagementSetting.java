7cb3a535cc9dbec2349209ebc8ac66c3e6dabf15
Hongjeongwoo <0721baby@gmail.com>
2018-07-13 16:36:40 +0900

    코인충전소, 메인화면, 장소빌려주기, 코인함, 버스커 유저 프로필 수정, 실시간 버스킹, 사용자 프로필 수정 버스킹생성, 버스킹생성_지도, 채널관리, 장소빌려주기 처음 시작할때, 장소검색, 장소 빌려주기 검색 결과 생성 이미지 추가

---
+++ app/src/main/java/graduationwork/buskingtown/ChannelManagementSetting.java
@@ -0,0 +1,13 @@
+package graduationwork.buskingtown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChannelManagementSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_channel_management_setting);
+    }
+}
