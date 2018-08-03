a4fa5696c305db39cfdaf8d955a509e5fafff058
minnn21 <minnn21@naver.com>
2018-08-03 19:05:59 +0900

    타임피커 생성

--- app/src/main/java/graduationwork/buskingtown/BuskingOpen.java
+++ app/src/main/java/graduationwork/buskingtown/BuskingOpen.java
@@ -2,11 +2,13 @@ package graduationwork.buskingtown;
 
 import android.app.DatePickerDialog;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import com.tsongkha.spinnerdatepicker.SpinnerDatePickerDialogBuilder;
 
@@ -18,8 +20,7 @@ import java.util.Locale;
 public class BuskingOpen extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, com.tsongkha.spinnerdatepicker.DatePickerDialog.OnDateSetListener {
     String concertDatePick;
 
-    int concertYear, concertMonth, concertDay;
-
+    int concertYear, concertMonth, concertDay, mHour, mMinute;
 
     SimpleDateFormat simpleDateFormat;
 
@@ -31,6 +32,12 @@ public class BuskingOpen extends AppCompatActivity implements DatePickerDialog.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busking_open);
+
+        //시간을 가져오기위한 Calendar 인스턴스 선언
+        Calendar cal = new GregorianCalendar();
+        mHour = cal.get(Calendar.HOUR_OF_DAY);
+        mMinute = cal.get(Calendar.MINUTE);
+
 
         concertDateButton = (Button) findViewById(R.id.concertStartDate);
 
@@ -51,6 +58,59 @@ public class BuskingOpen extends AppCompatActivity implements DatePickerDialog.O
             }
         });
     }
+
+    public void mOnClick(View v){
+        switch (v.getId()){
+            //시간 대화상자 버튼이 눌리면 대화상자를 보여줌
+
+            case R.id.timeStartDate:
+                //여기서 리스너도 등록함
+                new TimePickerDialog(BuskingOpen.this, startTimeSetListener, mHour, mMinute, false).show();
+
+                break;
+
+            case R.id.timeEndDate:
+                //여기서 리스너도 등록함
+                new TimePickerDialog(BuskingOpen.this, endTimeSetListener, mHour, mMinute, false).show();
+
+                break;
+        }
+    }
+
+    //시간 대화상자 리스너 부분
+
+    TimePickerDialog.OnTimeSetListener startTimeSetListener =
+
+            new TimePickerDialog.OnTimeSetListener() {
+
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    //사용자가 입력한 값을 가져온뒤
+                    mHour = hourOfDay;
+                    mMinute = minute;
+
+                    //텍스트뷰의 값을 업데이트함
+                    Button startD = (Button)findViewById(R.id.timeStartDate);
+                    startD.setText(String.format("%d:%d", mHour, mMinute));
+                }
+            };
+
+    TimePickerDialog.OnTimeSetListener endTimeSetListener =
+
+            new TimePickerDialog.OnTimeSetListener() {
+
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    //사용자가 입력한 값을 가져온뒤
+                    mHour = hourOfDay;
+                    mMinute = minute;
+
+                    //텍스트뷰의 값을 업데이트함
+                    Button endD = (Button)findViewById(R.id.timeEndDate);
+                    endD.setText(String.format("%d:%d", mHour, mMinute));
+                }
+
+            };
 
 
     void showDate(int year, int monthOfYear, int dayOfMonth, int spinnerTheme) {
