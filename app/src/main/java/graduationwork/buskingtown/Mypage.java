1abbb15751c70fd08babea69c4f3f06d4b948654
하성경 <gktjdrud@gmail.com>
2018-08-15 18:29:19 +0900

    mypage 연결

--- app/src/main/java/graduationwork/buskingtown/Mypage.java
+++ app/src/main/java/graduationwork/buskingtown/Mypage.java
@@ -32,14 +32,16 @@ public class Mypage extends Fragment {
 
     RelativeLayout go_Busker,coinLayout,coinhouseLayout,locationLendLayout,noticeLayout,clientcenterLayout,logout;
 
+    TextView go_Busker_text;
+
     public Mypage(){
         // Required empty public constructor
     }
 
     //유저 정보 변수들
     String user_token,user_name;
-    int user_id;
-    boolean certification;
+    int user_id,busker_id;
+    Boolean certification;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -72,6 +74,7 @@ public class Mypage extends Fragment {
 
         //여기다 링크 바로가기 선언하세요
         go_Busker = (RelativeLayout) v.findViewById(R.id.goBusker);
+        go_Busker_text = (TextView) getActivity().findViewById(R.id.goBuskerText);
         coinLayout = (RelativeLayout) v.findViewById(R.id.coinLayout);
         coinhouseLayout = (RelativeLayout) v.findViewById(R.id.coinhouseLayout);
         locationLendLayout = (RelativeLayout) v.findViewById(R.id.locationLendLyout);
@@ -133,17 +136,22 @@ public class Mypage extends Fragment {
                 setLogout();
             }
         });
+
         return v;
     }
 
+    //user 정보 불러오기
     public void getLocalData(){
         SharedPreferences pref = getActivity().getSharedPreferences("User", Activity.MODE_PRIVATE);
         user_token = pref.getString("auth_token",null);
         user_name = pref.getString("username",null);
         user_id = pref.getInt("user_id",0);
     }
-
-    public void buskerCheck(boolean certification, Busker busker){
+    /*
+    버스커 객체를 확인하여 null 값이면 BuskerCertification 액티비티를 띄우고
+    null값이 아니면 인증 상태를 확인하여 true > 버스커 채널 가기, false > 재시도, null > 대기 상태 액티비티로 이동
+    */
+    public void buskerCheck(Boolean certification, Busker busker){
         Log.e("인증상태-",String.valueOf(certification));
         Log.e("버스커-",String.valueOf(busker));
         if(busker == null){
@@ -155,19 +163,10 @@ public class Mypage extends Fragment {
                     startActivity(i);
                 }
             });
-        } else {
-            //내 채널 되기 가기
-            TextView go_Busker_text = (TextView) getActivity().findViewById(R.id.goBuskerText);
-            if(certification==true){
-                go_Busker_text.setText("내 채널 가기");
-                go_Busker.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(getActivity(),ChannelBusker.class);
-                        startActivity(i);
-                    }
-                });
-            }else if(certification==false){
+        }
+        else {
+            //내 채널 가기
+            if(certification == null){
                 go_Busker.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -175,10 +174,30 @@ public class Mypage extends Fragment {
                         startActivity(i);
                     }
                 });
+            } else {
+                if(certification == true){
+                    go_Busker_text.setText("내 채널 가기");
+                    go_Busker.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(getActivity(),ChannelBusker.class);
+                            startActivity(i);
+                        }
+                    });
+                }else if(certification == false){
+                    go_Busker.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(getActivity(),FailPass.class);
+                            startActivity(i);
+                        }
+                    });
+                }
             }
         }
     }
 
+    //버스커 객체를 확인하기 위함 busker 모델에서 인증 상태와 busker_id를 불러옴
     public void getBusker(String token,int id){
         final Busker[] busker = {new Busker()};
         final User[] userDetail = {new User()};
@@ -189,12 +208,15 @@ public class Mypage extends Fragment {
                 userDetail[0] = response.body();
                 if (userDetail[0].getBusker() != null) {
                     certification = userDetail[0].getBusker().getCertification();
+                    busker_id = userDetail[0].getBusker().getBusker_id();
                     if (response.isSuccessful()) {
                         Log.e("유저 아이디", String.valueOf(id));
+                        Log.e("버스커 아이디", String.valueOf(busker_id));
                         Log.e("인증상태", String.valueOf(certification));
                         Log.e("버스커", String.valueOf(busker));
                         Log.e("버스커유저정보가져오기:", "성공");
                         buskerCheck(certification, busker[0]);
+                        saveBuskerInfo(busker_id);
                     } else {
                         //에러 상태 보려고 해둔 코드
                         int StatusCode = response.code();
@@ -206,7 +228,7 @@ public class Mypage extends Fragment {
                         Log.e("리스폰스바디", String.valueOf(response.body()));
                     }
                 } else {
-                    buskerCheck(false, null);
+                    buskerCheck(null, null);
                 }
             }
 
@@ -217,14 +239,23 @@ public class Mypage extends Fragment {
         });
     }
 
+    //busker 정보를 저장하기 위함 key값 : BuskerUser
+    public void saveBuskerInfo(int busker_id){
+        SharedPreferences pref = getActivity().getSharedPreferences("BuskerUser", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("busker_id",busker_id);
+        editor.commit();
+    }
+
     public void restApiBuilder() {
         ApplicationController application = ApplicationController.getInstance();
         application.buildNetworkService();
         apiService = ApplicationController.getInstance().getRestApiService();
     }
 
+    //로그아웃 메소드
     public void setLogout(){
-        SharedPreferences pref = this.getActivity().getSharedPreferences(String.valueOf(getContext()),Context.MODE_PRIVATE);
+        SharedPreferences pref = getActivity().getSharedPreferences("User", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         editor.clear(); //clear all stored data
         editor.commit();
@@ -232,5 +263,6 @@ public class Mypage extends Fragment {
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        getActivity().finish();
     }
 }
