8b327f335490a87895883a64b05f2f66545c0f40
minnn21 <minnn21@naver.com>
2018-09-13 21:17:18 +0900

    포스트 모델

---
+++ app/src/main/java/graduationwork/buskingtown/model/Post.java
@@ -0,0 +1,49 @@
+package graduationwork.buskingtown.model;
+
+public class Post {
+    int post_id;
+    String content;
+    String busker;
+    String post_image;
+    String created_at;
+
+    public int getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(int post_id) {
+        this.post_id = post_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getBusker() {
+        return busker;
+    }
+
+    public void setBusker(String busker) {
+        this.busker = busker;
+    }
+
+    public String getPost_image() {
+        return post_image;
+    }
+
+    public void setPost_image(String post_image) {
+        this.post_image = post_image;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+}
