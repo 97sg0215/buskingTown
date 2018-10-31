9828cc49984b57b903866a8615e043ae51ea4a8b
Hongjeongwoo <0721baby@gmail.com>
2018-11-01 03:41:57 +0900

    예약취소 팝업창으로..

--- app/src/main/java/graduationwork/buskingtown/LocationCancel.java
+++ app/src/main/java/graduationwork/buskingtown/LocationCancel.java
@@ -1,13 +1,18 @@
 package graduationwork.buskingtown;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
-public class LocationCancel extends AppCompatActivity {
+public class LocationCancel extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_location_cancel);
     }
+
 }
