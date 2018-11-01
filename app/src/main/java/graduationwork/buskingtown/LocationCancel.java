2f55654384b9f5549cecaf49000cb7b1b2bc403f
Hongjeongwoo <0721baby@gmail.com>
2018-11-01 09:52:47 +0900

    예약취소 팝업창

--- app/src/main/java/graduationwork/buskingtown/LocationCancel.java
+++ app/src/main/java/graduationwork/buskingtown/LocationCancel.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import graduationwork.buskingtown.api.RestApiService;
@@ -15,7 +16,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class LocationCancel extends AppCompatActivity {
+public class LocationCancel extends Activity {
 
     RestApiService apiService;
     String user_token;
@@ -23,6 +24,7 @@ public class LocationCancel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_location_cancel);
 
         restApiBuilder();
