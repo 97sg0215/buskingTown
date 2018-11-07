ad49e71dc07966a59e15e4dcd5741e8d3d6e7eb3
Hongjeongwoo <0721baby@gmail.com>
2018-11-07 22:18:43 +0900

    엑스버튼 활성화

--- app/src/main/java/graduationwork/buskingtown/CoinSendAfter_pop.java
+++ app/src/main/java/graduationwork/buskingtown/CoinSendAfter_pop.java
@@ -3,14 +3,26 @@ package graduationwork.buskingtown;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
-public class CoinSendAfter_pop extends Activity {
+public class CoinSendAfter_pop extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_coin_send_after_pop);
+
+        findViewById(R.id.delete).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.delete:
+                this.finish();
+                break;
+        }
     }
 }
