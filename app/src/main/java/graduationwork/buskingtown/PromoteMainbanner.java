cb6106bdd1b9a463a2b99d1e7d704024318ec771
Hongjeongwoo <0721baby@gmail.com>
2018-11-12 04:55:56 +0900

    이메일 낫널~

--- app/src/main/java/graduationwork/buskingtown/PromoteMainbanner.java
+++ app/src/main/java/graduationwork/buskingtown/PromoteMainbanner.java
@@ -1,12 +1,20 @@
 package graduationwork.buskingtown;
 
 import android.app.DatePickerDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -16,7 +24,16 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
+import javax.mail.MessagingException;
+import javax.mail.SendFailedException;
+
 public class PromoteMainbanner extends Fragment {
+
+    private String email = "buskingtown2018@gmail.com";
+    private EditText message , phone_hint, email_hint, date_hint, content_hint, requriement_hint;
+    private Button paymentBtn = null;
+
+    String team_name, phone, user_email, date, total_message, content, requriement;
 
     TextView publicDateHint;
     SimpleDateFormat simpleDateFormat;
@@ -65,6 +82,144 @@ public class PromoteMainbanner extends Fragment {
             }
         });
 
+        //이메일 인터넷 사용 권한 허용
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .permitDiskReads()
+                .permitDiskWrites()
+                .permitNetwork().build());
+
+        //email = (EditText) v.findViewById(R.id.emailHint); //받는 사람의 이메일
+        message = (EditText) v.findViewById(R.id.teamNameHint); //본문 내용
+        phone_hint = (EditText) v.findViewById(R.id.phoneNumberHint);
+        email_hint = (EditText) v.findViewById(R.id.emailHint);
+        content_hint = (EditText)v.findViewById(R.id.publicContentHint);
+        requriement_hint = (EditText)v.findViewById(R.id.requriementHint);
+
+        message.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                team_name = message.getText().toString();
+            }
+        });
+
+        phone_hint.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        phone_hint.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                phone = phone_hint.getText().toString();
+            }
+        });
+
+        email_hint.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                user_email = email_hint.getText().toString();
+            }
+        });
+
+        content_hint.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                content = content_hint.getText().toString();
+            }
+        });
+
+        requriement_hint.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                requriement = requriement_hint.getText().toString();
+            }
+        });
+
+
+        total_message = "팀 이름: "+ team_name+"\n"
+                +"신청인 휴대폰: " + phone+"\n"
+                +"신청인 이메일: " + user_email+"\n"
+                +"신청 날짜: " ;
+
+        paymentBtn = (Button) v.findViewById(R.id.paymentBtn);
+        paymentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new senmailAsync().execute();
+            }
+        });
+
         return v;
     }
+    private class senmailAsync extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                GMailSender gMailSender = new GMailSender("buskingtown2018@gmail.com", "khphTown123");
+                //GMailSender.sendMail(제목, 본문내용, 받는사람);
+                gMailSender.sendMail("추천순위 노출 신청입니다.", total_message, email);
+                Log.e("메시지",String.valueOf(message));
+                Log.e("이메일",String.valueOf(email));
+                Log.e("이메일","이메일을 성공적으로 보냈습니다.");
+                //  Toast.makeText(getActivity().getApplicationContext(), "이메일을 성공적으로 보냈습니다.", Toast.LENGTH_SHORT).show();
+            } catch (SendFailedException e) {
+                Log.e("이메일","이메일 형식이 잘못되었습니다.");
+                //   Toast.makeText(getActivity().getApplicationContext(), "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+            } catch (MessagingException e) {
+                Log.e("이메일","인터넷 연결을 확인해주십시오");
+                // Toast.makeText(getActivity().getApplicationContext(), "인터넷 연결을 확인해주십시오", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
